37f8a0e0375c3a1a6b15a99bcee55eae599ed143
Ken Waribo <degee147@gmail.com>
2019-10-24 10:22:00 +0100

    fixed description issue with simple pie

--- daily.php
+++ daily.php
@@ -3,6 +3,15 @@ ini_set("log_errors", 1);
 ini_set("error_log", "./error.log");
 
 include 'onesignal.php';
+// require __DIR__ . '/vendor/autoload.php';
+// require_once('./php/autoloader.php');
+
+include_once('simplepie/autoloader.php');
+
+include_once('simplepie/idn/idna_convert.class.php');
+
+include_once('simple_html_dom.php');
+
 
 function getFirstImg($html)
 {
@@ -21,6 +30,184 @@ function getFirstImg($html)
     return $images[0];
 }
 
+function getFeed($rssurl)
+{
+    if (isset($_COOKIE['rss_post_links'])) {
+        $rss_post_links = json_decode($_COOKIE['rss_post_links'], true);
+    } else {
+        $rss_post_links = array();
+    }
+
+    $returnArr = array();
+
+    $returnArr['success'] = 0;
+
+    $returnArr['msg'] = 'Something wrong';
+
+    $returnArr['data'] = array();
+
+    //$rssurl = 'http://rss.cnn.com/rss/edition.rss';
+
+    //$rssurl = 'http://mashable.com/social-media/feed/';
+
+
+    try {
+
+        // Create a new instance of the SimplePie object
+
+        $feed = new SimplePie();
+
+        // Use the URL that was passed to the page in SimplePie
+
+        $feed->set_feed_url($rssurl);
+
+        // Trigger force-feed
+
+        $feed->force_feed(true);
+
+        // Initialize the whole SimplePie object.  Read the feed, process it, parse it, cache it, and
+
+        // all that other good stuff.  The feed's information will not be available to SimplePie before
+
+        // this is called.
+
+        $success = $feed->init();
+
+        if ($feed->error()) {
+
+            $returnArr['success'] = 0;
+
+            $returnArr['msg'] = $feed->error();
+
+            return json_encode($returnArr);
+        }
+
+
+        // We'll make sure that the right content type and character encoding gets set automatically.
+
+        // This function will grab the proper character encoding, as well as set the content type to text/html.
+
+        $feed->handle_content_type();
+
+        $returnArr = array();
+
+        $items = $feed->get_items();
+
+        $content = '';
+
+        $img = '';
+
+        $gotUniqueData = false;
+
+        foreach ($items as $item) {
+
+            if ($gotUniqueData)
+                break;
+
+            $title = $item->get_title();
+
+            $content = ($item->get_content()) ? $item->get_content() : $item->get_description();
+
+            // Check for enclosures.  If an item has any, set the first one to the $enclosure variable.
+
+            if ($enclosure = $item->get_enclosure(0)) {
+
+                if ($enclosure->get_thumbnail()) {
+
+                    $img = $enclosure->get_thumbnail();
+                } else if ($content) {
+
+                    $description = $content;
+
+                    $desc_dom = str_get_html($description);
+
+                    $image = $desc_dom->find('img', 0);
+
+                    if (isset($image->src))
+                        $img = $image->src;
+                    else
+                        $img = 'https://suite.social/apps/notify/admin/suite_80px.jpg';
+                }
+            }
+
+            $link = $item->get_permalink();
+
+            if (!in_array($link, $rss_post_links)) {
+
+                $rss_post_links[] = $link;
+
+                setcookie("rss_post_links", json_encode($rss_post_links));
+
+                $gotUniqueData = true;
+            }
+        }
+
+        $content = str_replace('"', "'", $content);  //double quotes for mailto: emails.
+
+        $strip_tags = trim(strip_tags($content));
+
+        $content = trim($content);
+
+
+        if ($gotUniqueData) {
+
+            $returnData = array();
+
+            $message2 = ($strip_tags) ? $strip_tags : $content;
+
+            $message2 = substr($message2, 0, 350);
+
+            $returnData['title'] = $title;
+
+            $returnData['message'] = $message2;
+
+            $returnData['link'] = $link;
+
+            $returnData['img'] = $img;
+        } else {
+            $returnData = array();
+
+            $message2 = ($strip_tags) ? $strip_tags : $content;
+
+            $message2 = substr($message2, 0, 350);
+
+            $returnData['title'] = $title;
+
+            $returnData['message'] = $message2;
+
+            $returnData['link'] = $link;
+
+            $returnData['img'] = $img;
+
+            $rss_post_links = array();
+
+            $rss_post_links[] = $link;
+
+            setcookie("rss_post_links", json_encode($rss_post_links));
+        }
+
+
+        $returnArr['success'] = 1;
+
+        $returnArr['msg'] = 'got data from rss feed';
+
+        $returnArr['data'] = $returnData;
+        return $returnArr;
+    } catch (Exception $exc) {
+
+        return $exc->getTraceAsString();
+    }
+}
+
+//receives a simple pie item
+function getImage($item)
+{
+    $htmlDOM = new simple_html_dom();
+    $htmlDOM->load($item->get_content());
+    $image = $htmlDOM->find('img', 0);
+    return $image->src;
+}
+
 function runschedule()
 {
     include 'config.php';
@@ -33,49 +220,37 @@ function runschedule()
 
     if ($schedule['status'] == "start") {
 
-        $xml = simplexml_load_string(file_get_contents($schedule['feed']));
-        // file_put_contents('./xml.json', file_get_contents($schedule['feed']));
-        $json = json_encode($xml);
-        $array = json_decode($json, TRUE);
+
+        $feed = new SimplePie();
+        $feed->set_feed_url($schedule['feed']);
+        $feed->init();
+        $feed->handle_content_type();
 
         $sent_posts = [];
-
-        // var_dump($array);
-        // die();
 
         if (!empty($schedule['sent_posts'])) {
             $sent_posts = $schedule['sent_posts'];
         }
 
-        foreach ($array['channel']['item'] as $item) {
-            if (!in_array($item['pubDate'], $sent_posts)) {
-                $sent_posts[] = $item['pubDate'];
 
-                $title = $item['title'];
-                $content = !empty($item['description']) ? $item['description'] : "";
-                $url = $item['link'];
-                // $url = $item['link'];
-                // $chrome_web_icon = getFirstImg($item);
-                // var_dump($chrome_web_icon);
-                // var_dump($chrome_web_icon);
-                // die();
+        foreach ($feed->get_items() as $item) {
+            // var_dump($item->get_date());
+            
+            if (!in_array($item->get_date(), $sent_posts)) {
+                $sent_posts[] = $item->get_date();
 
-                $res = $on->sendDaily($title, $content, $url, $schedule['time'], "", $lang = 'en');
+                $title = $item->get_title();
+                $content = !empty($item->get_description()) ? $item->get_description() : "";
+                $url = !empty($item->get_link()) ? $item->get_link() : "";
+                $chrome_web_icon = !empty(getImage($item)) ? getImage($item) : "";;
+
+                $res = $on->sendDaily($title, $content, $url, $schedule['time'], $chrome_web_icon);
                 break;
             }
         }
 
 
         $schedule['sent_posts'] = $sent_posts;
-
-        // die();
-
         file_put_contents('./schedule.json', json_encode($schedule));
-
-
-
-
-        // $book = json_decode($res, true);
-        // echo $book['recipients'];
     }
 }
--- onesignal.php
+++ onesignal.php
@@ -1,43 +1,46 @@
 <?php
 
-class OneSignal{
+class OneSignal
+{
 	private $app_id;
 	private $auth_key;
 	private $api_logo;
-	public function __construct($app_id, $auth_key, $api_logo){
+	public function __construct($app_id, $auth_key, $api_logo)
+	{
 		$this->app_id = $app_id;
 		$this->auth_key = $auth_key;
 		$this->api_logo = $api_logo;
 	}
-	
-	public function singleMessage($title, $content, $url, $userid, $lang = 'en'){
-		
+
+	public function singleMessage($title, $content, $url, $userid, $lang = 'en')
+	{
+
 		$content = array(
 			$lang => $content,
 		);
-			
+
 		$heading = array(
 			$lang => $title,
 		);
-		
+
 		$fields = array(
 			'app_id' => $this->app_id,
 			'included_segments' => array('Active Users'),
-			'include_player_ids' => array( $userid ),
+			'include_player_ids' => array($userid),
 			'contents' => $content,
 			'url' => $url,
 			'headings' => $heading,
 			'chrome_web_icon' => $this->api_logo,
 		);
-		
+
 		$headers = array(
 			'Content-Type: application/json; charset=utf-8',
-			'Authorization: Basic '.$this->auth_key,
+			'Authorization: Basic ' . $this->auth_key,
 		);
-		
+
 		$fields = json_encode($fields);
 
-		
+
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -49,20 +52,21 @@ class OneSignal{
 
 		$response = curl_exec($ch);
 		curl_close($ch);
-		
-		return $response;
-	}	
 
-	public function sendMessage($title, $content, $url, $lang = 'en'){
-		
+		return $response;
+	}
+
+	public function sendMessage($title, $content, $url, $lang = 'en')
+	{
+
 		$content = array(
 			$lang => $content,
 		);
-			
+
 		$heading = array(
 			$lang => $title,
 		);
-		
+
 		$fields = array(
 			'app_id' => $this->app_id,
 			'included_segments' => array('Active Users', 'Subscribed Users'),
@@ -72,15 +76,15 @@ class OneSignal{
 			'headings' => $heading,
 			'chrome_web_icon' => $this->api_logo,
 		);
-		
+
 		$headers = array(
 			'Content-Type: application/json; charset=utf-8',
-			'Authorization: Basic '.$this->auth_key,
+			'Authorization: Basic ' . $this->auth_key,
 		);
-		
+
 		$fields = json_encode($fields);
 
-		
+
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -92,20 +96,21 @@ class OneSignal{
 
 		$response = curl_exec($ch);
 		curl_close($ch);
-		
-		return $response;
-	}	
 
-	public function sendAfter($title, $content, $url, $sendaft, $lang = 'en'){
-		
+		return $response;
+	}
+
+	public function sendAfter($title, $content, $url, $sendaft, $lang = 'en')
+	{
+
 		$content = array(
 			$lang => $content,
 		);
-			
+
 		$heading = array(
 			$lang => $title,
 		);
-		
+
 		$fields = array(
 			'app_id' => $this->app_id,
 			'included_segments' => array('Active Users', 'Subscribed Users'),
@@ -117,16 +122,16 @@ class OneSignal{
 			'send_after' => $sendaft
 		);
 
-		
-		
+
+
 		$headers = array(
 			'Content-Type: application/json; charset=utf-8',
-			'Authorization: Basic '.$this->auth_key,
+			'Authorization: Basic ' . $this->auth_key,
 		);
-		
+
 		$fields = json_encode($fields);
 
-		
+
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -138,7 +143,7 @@ class OneSignal{
 
 		$response = curl_exec($ch);
 		// var_dump($response);
-		
+
 		$err = curl_error($ch);
 		// var_dump($err);
 		// die();
@@ -148,22 +153,23 @@ class OneSignal{
 
 		// var_dump($response);
 		// die();
-		
-		return $response;
-	}	
 
-	
-	public function sendDaily($title, $content, $url, $time, $chrome_web_icon = "", $lang = 'en'){
-		
-		
+		return $response;
+	}
+
+
+	public function sendDaily($title, $content, $url, $time, $chrome_web_icon, $lang = 'en')
+	{
 		$content = array(
 			$lang => $content,
 		);
-			
+
 		$heading = array(
 			$lang => $title,
 		);
-		
+
+		$icon = !empty($chrome_web_icon) ? $chrome_web_icon : $this->api_logo;
+
 		$fields = array(
 			'app_id' => $this->app_id,
 			// 'included_segments' => array('Active Users', 'Subscribed Users'),
@@ -172,22 +178,22 @@ class OneSignal{
 			'contents' => $content,
 			'url' => $url,
 			'headings' => $heading,
-            'chrome_web_icon' => $this->api_logo,
-            'delayed_option' => 'timezone',
-            'delivery_time_of_day' => $time,
+			'chrome_web_icon' => $icon,
+			'delayed_option' => 'timezone',
+			'delivery_time_of_day' => $time,
 			// 'send_after' => $sendaft
 		);
 
-		
-		
+
+
 		$headers = array(
 			'Content-Type: application/json; charset=utf-8',
-			'Authorization: Basic '.$this->auth_key,
+			'Authorization: Basic ' . $this->auth_key,
 		);
-		
+
 		$fields = json_encode($fields);
 
-		
+
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -199,7 +205,7 @@ class OneSignal{
 
 		$response = curl_exec($ch);
 		// var_dump($response);
-		
+
 		$err = curl_error($ch);
 		// var_dump($err);
 		// die();
@@ -209,7 +215,7 @@ class OneSignal{
 
 		// var_dump($response);
 		// die();
-		
+
 		return $response;
-	}	
+	}
 }
